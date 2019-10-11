9b7659fb84319a85f8eafb921a9f006ff3eefbb7
lasicin <LaercioSilvaFerracini@correio.itau.com.br>
2019-10-11 10:56:15 -0300

    NEW:  ADICIONADO TESTE DE SCROLL

--- src/main/java/AppTest.java
+++ src/main/java/AppTest.java
@@ -1,9 +1,15 @@
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.springframework.context.annotation.Description;
 import org.testng.Assert;
@@ -35,10 +41,11 @@ public class AppTest {
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "12.1");
 //        capabilities.setCapability(MobileCapabilityType.APP, "/Users/lasicin/IdeaProjects/iosteste/Teste/build/Release-iphonesimulator/Teste.app");
-        capabilities.setCapability(MobileCapabilityType.APP, "/Users/lasicin/IdeaProjects/iosteste/TabTeste/build/Release-iphonesimulator/TabTeste.app");
-        capabilities.setCapability(MobileCapabilityType.NO_RESET, true);
+//        capabilities.setCapability(MobileCapabilityType.APP, "/Users/lasicin/IdeaProjects/iosteste/TabTeste/build/Release-iphonesimulator/TabTeste.app");
+        capabilities.setCapability("bundleId", "com.apple.Preferences");
+        capabilities.setCapability(MobileCapabilityType.NO_RESET, false);
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
-        capabilities.setCapability("showIOSLog", true);
+        capabilities.setCapability("showIOSLog", false);
         capabilities.setCapability("useNewWDA", false);
         //4
         driver = new IOSDriver<IOSElement>(url, capabilities);
@@ -47,9 +54,22 @@ public class AppTest {
     }
 
     //5
-    @AfterSuite
-    public void uninstallApp() throws InterruptedException {
-        driver.removeApp("br.com.teste");
+//    @AfterSuite
+//    public void uninstallApp() throws InterruptedException {
+//        driver.removeApp("br.com.teste");
+//    }
+
+    @Test
+    public void scrollTest() {
+        driver.findElement(MobileBy.iOSNsPredicateString("type == 'XCUIElementTypeStaticText' and name CONTAINS[c] 'Desenvolvedor' ")).click();
+//        driver.findElement(MobileBy.iOSNsPredicateString("type == 'XCUIElementTypeStaticText' and name CONTAINS[c] 'Sobre' ")).click();
+//        driver.findElement(MobileBy.iOSNsPredicateString("type == 'XCUIElementTypeStaticText' and name CONTAINS[c] 'Legal' ")).click();
+//        driver.findElement(MobileBy.iOSNsPredicateString("type == 'XCUIElementTypeStaticText' and name CONTAINS[c] 'Aviso' ")).click();
+        HashMap<String, String> scrollObject = new HashMap<>();
+        scrollObject.put("predicateString", "value CONTAINS[c] 'TV Providers'");
+        driver.executeScript("mobile: scroll", scrollObject);
+        driver.findElement(MobileBy.iOSNsPredicateString("type == 'XCUIElementTypeStaticText' and name CONTAINS[c] 'TV Providers' ")).getText();
+        Assert.assertEquals("TV Providers", driver.findElement(MobileBy.iOSNsPredicateString("type == 'XCUIElementTypeStaticText' and name CONTAINS[c] 'TV Providers' ")).getText());
     }
 
     //6
@@ -58,36 +78,77 @@ public class AppTest {
         Map<String, Object> status = driver.getStatus();
 
         for (Map.Entry e : status.entrySet()) {
-            System.out.printf("Driver Status Key: %s -> Value: %s %n",e.getKey(),e.getValue());
+            System.out.printf("Driver Status Key: %s -> Value: %s %n", e.getKey(), e.getValue());
         }
 
         Map<String, Object> settings = driver.getSettings();
 
         for (Map.Entry e : status.entrySet()) {
-            System.out.printf("settings Key: %s -> Value: %s %n",e.getKey(),e.getValue());
+            System.out.printf("settings Key: %s -> Value: %s %n", e.getKey(), e.getValue());
         }
 
         List<Map<String, Object>> allSessionDetails = driver.getAllSessionDetails();
 
-        for (Map<String, Object> m: allSessionDetails){
-            for (Map.Entry e: m.entrySet()){
-                System.out.printf("allSessionDetails Key: %s -> Value: %s %n",e.getKey(),e.getValue());
+        for (Map<String, Object> m : allSessionDetails) {
+            for (Map.Entry e : m.entrySet()) {
+                System.out.printf("allSessionDetails Key: %s -> Value: %s %n", e.getKey(), e.getValue());
             }
         }
         System.out.printf("getPageSource Key: %s %n", driver.getPageSource());
 
         driver.resetApp();
     }
+
     @Test(description = "Clicar na segunda aba e validar o texto")
-    public void acessarSegundaAba(){
-        driver.findElementByAccessibilityId("Second").click();
-        Assert.assertEquals("Second View",driver.findElementByAccessibilityId("Second View").getText());
+    public void acessarSegundaAba() {
+//        driver.findElementByAccessibilityId("Second").click();
+//        Assert.assertEquals("Second View", driver.findElementByAccessibilityId("Second View").getText());
     }
 
     @Test(description = "Clicar na primeira aba e validar o texto")
     @Description("Clicar na primeira aba e validar o texto")
-    public void acessarPrimeiraAba(){
-        driver.findElementByAccessibilityId("First").click();
-        Assert.assertEquals("First View",driver.findElementByAccessibilityId("First View").getText());
+    public void acessarPrimeiraAba() {
+//        driver.findElementByAccessibilityId("First").click();
+//        Assert.assertEquals("First View", driver.findElementByAccessibilityId("First View").getText());
+    }
+
+    @Test
+    @Description("Terminate app and activate App")
+    public void teste() {
+//        driver.terminateApp("br.com.teste");
+//        driver.unlockDevice();
+//        driver.activateApp("br.com.teste");
+    }
+
+    private void showNotifications() {
+        manageNotifications(true);
+    }
+
+    private void hideNotifications() {
+        manageNotifications(false);
+    }
+
+    private void manageNotifications(Boolean show) {
+
+        Dimension screenSize = driver.manage().window().getSize();
+
+        int yMargin = 5;
+        int xMid = screenSize.width / 2;
+        PointOption top = PointOption.point(xMid, yMargin);
+        PointOption bottom = PointOption.point(xMid, screenSize.height - yMargin);
+
+        TouchAction action = new TouchAction(driver);
+        if (show) {
+            action.press(top);
+        } else {
+            action.press(bottom);
+        }
+        action.waitAction(WaitOptions.waitOptions(Duration.ofSeconds(1)));
+        if (show) {
+            action.moveTo(bottom);
+        } else {
+            action.moveTo(top);
+        }
+        action.perform();
     }
 }
